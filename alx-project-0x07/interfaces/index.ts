f46b1ea3f8fa87fc3b3857830182eb39799fb419
Dagi <dagmawimilkias@gmail.com>
2025-01-18 21:38:58 +0300

    Image Generator App

--- alx-project-0x07/interfaces/index.ts
+++ alx-project-0x07/interfaces/index.ts
@@ -2,3 +2,10 @@ import React from "react";
 export interface ReactComponentProps {
   children: React.ReactNode;
 }
+export interface GeneratedImagesProps {
+  imageURL: string;
+  prompt: string;
+  width?: string;
+  height?: string;
+}
+export type ImageProps = Pick<GeneratedImagesProps, "imageURL" | "prompt">;
